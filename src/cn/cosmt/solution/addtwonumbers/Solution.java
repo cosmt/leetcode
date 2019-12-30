f0e0eb953b518ed2c1851cf83ed0ac174f6642ec
ztf <ztf@cosmt.cn>
2019-12-30 20:17:32 +0800

    4.寻找两个有序数组的中位数

--- src/cn/cosmt/solution/addtwonumbers/Solution.java
+++ src/cn/cosmt/solution/addtwonumbers/Solution.java
@@ -17,7 +17,7 @@ package cn.cosmt.solution.addtwonumbers;
  * 链接：https://leetcode-cn.com/problems/add-two-numbers
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class AddTwoNumbers {
+public class Solution {
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
--- src/cn/cosmt/solution/lengthOfLongestSubstring/Solution.java
+++ src/cn/cosmt/solution/lengthOfLongestSubstring/Solution.java
@@ -31,7 +31,7 @@ import java.util.Set;
  * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class LengthOfLongestSubstring {
+public class Solution {
 
 
     //    简单处理的
